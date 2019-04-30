02306e8897cb9441e994c0f5b47ca004af111e6f
Rikki Pitt <rikkipitt@gmail.com>
2019-04-30 12:16:48 +0100

    Update to Harvest v2

--- lib/omniauth/strategies/harvest.rb
+++ lib/omniauth/strategies/harvest.rb
@@ -1,36 +1,32 @@
-require 'omniauth/strategies/oauth2'
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
     class Harvest < OmniAuth::Strategies::OAuth2
-
-      option :client_options, {
-        :site => 'https://api.harvestapp.com',
-        :authorize_url => '/oauth2/authorize',
-        :token_url => '/oauth2/token'
-      }
+      option :client_options,
+             site: "https://id.getharvest.com",
+             authorize_url: "/oauth2/authorize",
+             token_url: "/api/v2/oauth2/token"
 
       uid { raw_info["user"]["id"] }
 
       info do
         {
-          :email => raw_info["user"]["email"],
-          :first_name => raw_info["user"]["first_name"],
-          :last_name => raw_info["user"]["last_name"],
-          :image => raw_info["user"]["avatar_url"]
+          email: raw_info["user"]["email"],
+          first_name: raw_info["user"]["first_name"],
+          last_name: raw_info["user"]["last_name"],
         }
       end
 
       extra do
         {
-          :raw_info => raw_info
+          raw_info: raw_info["accounts"]
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/account/who_am_i.json").parsed
+        @raw_info ||= access_token.get("/api/v2/accounts").parsed
       end
-
     end
   end
 end
